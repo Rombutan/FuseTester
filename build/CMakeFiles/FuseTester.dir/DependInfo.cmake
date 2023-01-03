605add9b17cb1fbf5a717da8d693d6ba81271dd6
Luca Younes <lucaguyyounes@gmail.com>
2023-01-03 11:52:37 -0800

    setup build env

---
+++ build/CMakeFiles/FuseTester.dir/DependInfo.cmake
@@ -0,0 +1,157 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_divider/divider.S" "/home/rombutan/Documents/FuseTester/build/CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_divider/divider.S.o"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_irq/irq_handler_chain.S" "/home/rombutan/Documents/FuseTester/build/CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_irq/irq_handler_chain.S.o"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_bit_ops/bit_ops_aeabi.S" "/home/rombutan/Documents/FuseTester/build/CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_bit_ops/bit_ops_aeabi.S.o"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_divider/divider.S" "/home/rombutan/Documents/FuseTester/build/CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_divider/divider.S.o"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_double/double_aeabi.S" "/home/rombutan/Documents/FuseTester/build/CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_double/double_aeabi.S.o"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_double/double_v1_rom_shim.S" "/home/rombutan/Documents/FuseTester/build/CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_double/double_v1_rom_shim.S.o"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_float/float_aeabi.S" "/home/rombutan/Documents/FuseTester/build/CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_float/float_aeabi.S.o"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_float/float_v1_rom_shim.S" "/home/rombutan/Documents/FuseTester/build/CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_float/float_v1_rom_shim.S.o"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_int64_ops/pico_int64_ops_aeabi.S" "/home/rombutan/Documents/FuseTester/build/CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_int64_ops/pico_int64_ops_aeabi.S.o"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_mem_ops/mem_ops_aeabi.S" "/home/rombutan/Documents/FuseTester/build/CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_mem_ops/mem_ops_aeabi.S.o"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_standard_link/crt0.S" "/home/rombutan/Documents/FuseTester/build/CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_standard_link/crt0.S.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "LIB_PICO_BIT_OPS=1"
+  "LIB_PICO_BIT_OPS_PICO=1"
+  "LIB_PICO_DIVIDER=1"
+  "LIB_PICO_DIVIDER_HARDWARE=1"
+  "LIB_PICO_DOUBLE=1"
+  "LIB_PICO_DOUBLE_PICO=1"
+  "LIB_PICO_FLOAT=1"
+  "LIB_PICO_FLOAT_PICO=1"
+  "LIB_PICO_INT64_OPS=1"
+  "LIB_PICO_INT64_OPS_PICO=1"
+  "LIB_PICO_MALLOC=1"
+  "LIB_PICO_MEM_OPS=1"
+  "LIB_PICO_MEM_OPS_PICO=1"
+  "LIB_PICO_PLATFORM=1"
+  "LIB_PICO_PRINTF=1"
+  "LIB_PICO_PRINTF_PICO=1"
+  "LIB_PICO_RUNTIME=1"
+  "LIB_PICO_STANDARD_LINK=1"
+  "LIB_PICO_STDIO=1"
+  "LIB_PICO_STDIO_UART=1"
+  "LIB_PICO_STDLIB=1"
+  "LIB_PICO_SYNC=1"
+  "LIB_PICO_SYNC_CORE=1"
+  "LIB_PICO_SYNC_CRITICAL_SECTION=1"
+  "LIB_PICO_SYNC_MUTEX=1"
+  "LIB_PICO_SYNC_SEM=1"
+  "LIB_PICO_TIME=1"
+  "LIB_PICO_UTIL=1"
+  "PICO_BOARD=\"pico\""
+  "PICO_BUILD=1"
+  "PICO_CMAKE_BUILD_TYPE=\"Release\""
+  "PICO_COPY_TO_RAM=0"
+  "PICO_CXX_ENABLE_EXCEPTIONS=0"
+  "PICO_NO_FLASH=0"
+  "PICO_NO_HARDWARE=0"
+  "PICO_ON_DEVICE=1"
+  "PICO_TARGET_NAME=\"FuseTester\""
+  "PICO_USE_BLOCKED_RAM=0"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "."
+  "/home/rombutan/pico-sdk/src/common/pico_stdlib/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_gpio/include"
+  "/home/rombutan/pico-sdk/src/common/pico_base/include"
+  "generated/pico_base"
+  "/home/rombutan/pico-sdk/src/boards/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_platform/include"
+  "/home/rombutan/pico-sdk/src/rp2040/hardware_regs/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_base/include"
+  "/home/rombutan/pico-sdk/src/rp2040/hardware_structs/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_claim/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_sync/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_irq/include"
+  "/home/rombutan/pico-sdk/src/common/pico_sync/include"
+  "/home/rombutan/pico-sdk/src/common/pico_time/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_timer/include"
+  "/home/rombutan/pico-sdk/src/common/pico_util/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_uart/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_divider/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_runtime/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_clocks/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_resets/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_pll/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_vreg/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_watchdog/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_xosc/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_printf/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_bootrom/include"
+  "/home/rombutan/pico-sdk/src/common/pico_bit_ops/include"
+  "/home/rombutan/pico-sdk/src/common/pico_divider/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_double/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_int64_ops/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_float/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_malloc/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/boot_stage2/include"
+  "/home/rombutan/pico-sdk/src/common/pico_binary_info/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_stdio/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_stdio_uart/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_pio/include"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_i2c/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/rombutan/pico-sdk/src/common/pico_sync/critical_section.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_sync/critical_section.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_sync/critical_section.c.o.d"
+  "/home/rombutan/pico-sdk/src/common/pico_sync/lock_core.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_sync/lock_core.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_sync/lock_core.c.o.d"
+  "/home/rombutan/pico-sdk/src/common/pico_sync/mutex.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_sync/mutex.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_sync/mutex.c.o.d"
+  "/home/rombutan/pico-sdk/src/common/pico_sync/sem.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_sync/sem.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_sync/sem.c.o.d"
+  "/home/rombutan/pico-sdk/src/common/pico_time/time.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_time/time.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_time/time.c.o.d"
+  "/home/rombutan/pico-sdk/src/common/pico_time/timeout_helper.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_time/timeout_helper.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_time/timeout_helper.c.o.d"
+  "/home/rombutan/pico-sdk/src/common/pico_util/datetime.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_util/datetime.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_util/datetime.c.o.d"
+  "/home/rombutan/pico-sdk/src/common/pico_util/pheap.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_util/pheap.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_util/pheap.c.o.d"
+  "/home/rombutan/pico-sdk/src/common/pico_util/queue.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_util/queue.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/common/pico_util/queue.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_claim/claim.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_claim/claim.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_claim/claim.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_clocks/clocks.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_clocks/clocks.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_clocks/clocks.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_gpio/gpio.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_gpio/gpio.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_gpio/gpio.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_i2c/i2c.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_i2c/i2c.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_i2c/i2c.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_irq/irq.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_irq/irq.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_irq/irq.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_pio/pio.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_pio/pio.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_pio/pio.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_pll/pll.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_pll/pll.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_pll/pll.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_sync/sync.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_sync/sync.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_sync/sync.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_timer/timer.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_timer/timer.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_timer/timer.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_uart/uart.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_uart/uart.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_uart/uart.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_vreg/vreg.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_vreg/vreg.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_vreg/vreg.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_watchdog/watchdog.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_watchdog/watchdog.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_watchdog/watchdog.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/hardware_xosc/xosc.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_xosc/xosc.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/hardware_xosc/xosc.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_bootrom/bootrom.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_bootrom/bootrom.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_bootrom/bootrom.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_double/double_init_rom.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_double/double_init_rom.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_double/double_init_rom.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_double/double_math.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_double/double_math.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_double/double_math.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_float/float_init_rom.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_float/float_init_rom.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_float/float_init_rom.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_float/float_math.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_float/float_math.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_float/float_math.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_malloc/pico_malloc.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_malloc/pico_malloc.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_malloc/pico_malloc.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_platform/platform.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_platform/platform.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_platform/platform.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_printf/printf.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_printf/printf.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_printf/printf.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_runtime/runtime.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_runtime/runtime.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_runtime/runtime.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_stdio/stdio.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_stdio/stdio.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_stdio/stdio.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_stdio_uart/stdio_uart.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_stdio_uart/stdio_uart.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_stdio_uart/stdio_uart.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_stdlib/stdlib.c" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_stdlib/stdlib.c.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_stdlib/stdlib.c.o.d"
+  "/home/rombutan/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp.o" "gcc" "CMakeFiles/FuseTester.dir/home/rombutan/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp.o.d"
+  "/home/rombutan/Documents/FuseTester/main.cpp" "CMakeFiles/FuseTester.dir/main.cpp.o" "gcc" "CMakeFiles/FuseTester.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/FuseTester.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for FuseTester.
+# This may be replaced when dependencies are built.
